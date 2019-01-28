bf00ac9f97f67dc9baea7e9cec5f3865a602146f
DavidRussPrater <davidrussprater@gmail.com>
2019-01-28 14:08:26 -0700

    Added a temporary fix for crash caused when earthquake data is added to map.

--- app/src/main/java/com/example/prate/coloradogeology/EarthquakeMapsFragment.java
+++ app/src/main/java/com/example/prate/coloradogeology/EarthquakeMapsFragment.java
@@ -1,6 +1,7 @@
 package com.example.prate.coloradogeology;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,7 +15,7 @@ import static com.example.prate.coloradogeology.QueryUtils.locationArray;
 import static com.example.prate.coloradogeology.QueryUtils.longitudeArray;
 import static com.example.prate.coloradogeology.QueryUtils.magnitudeArray;
 
-public class EarthquakeMapsFragment extends SupportMapFragment implements OnMapReadyCallback  {
+public class EarthquakeMapsFragment extends SupportMapFragment implements OnMapReadyCallback, AsyncResponse  {
 
     private GoogleMap mMap;
 
@@ -36,8 +37,6 @@ public class EarthquakeMapsFragment extends SupportMapFragment implements OnMapR
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -46,9 +45,40 @@ public class EarthquakeMapsFragment extends SupportMapFragment implements OnMapR
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
+
         // Define the Long and Lat of Colorado and move the camera here
         LatLng colorado = new LatLng(39.5501, -105.7821);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(colorado));
+
+
+
+        // Make the camera zoom in to show the full state of Colorado
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(colorado, 6), 2000, null);
+
+        finish();
+
+    }
+
+    // TODO Find a way to make the map to wait and load only after the data has been parsed from the network
+
+    public void finish() {
+
+        /* Put the thread to sleep momentarily to give the earthquake data time to load then add
+         * markers for the the earthquakes to the map
+         *
+         * Other wise the app crashes for null pointer exception in QueryUtils because it is attempting
+         * to  split the coordinates data before it has loaded.
+         *
+         * This is not best practice but its a temporary fix
+         */
+
+        try {
+            Thread.sleep(600);
+            System.out.print("Sleeping");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
         // This for loop displays the fifteen most recent earthquakes in Colorado
         for (int i = 0; i < 15; i++) {
@@ -56,6 +86,7 @@ public class EarthquakeMapsFragment extends SupportMapFragment implements OnMapR
             // Get the string value for the earthquakes longitude at index i and convert it from a String
             // to a double to display on the map
             String finalLongitudeString = longitudeArray[i];
+            System.out.println("Coordinates: " + finalLongitudeString);
             double longitudeDouble = Double.parseDouble(finalLongitudeString);
 
             // Get the string value for the earthquakes latitude at index i and convert it from a String
@@ -69,10 +100,6 @@ public class EarthquakeMapsFragment extends SupportMapFragment implements OnMapR
             mMap.addMarker(new MarkerOptions().position(earthquake).title(locationArray[i] + " Magnitude: " + magnitudeArray[i]));
 
         }
-
-        // Make the camera zoom in to show the full state of Colorado
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(colorado, 6), 2000, null);
-
 
     }
 }
